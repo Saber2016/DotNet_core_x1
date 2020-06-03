3503652fcadc1b4a26446d3695daf517e70db907
saber <956441594@qq.com>
2020-06-03 13:59:46 +0800

    add a example

---
+++ MysqlEntityFramework/MysqlEntityFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MysqlEntityFramework.test1;
+
+
+namespace MysqlEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            NewTable new1 = new NewTable
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "sanmao",
+                Age = 23,
+                Gender = 1,
+                Hight = 157,
+                Weight = 50,
+                Time = DateTime.Now
+            };
+            TestModel.AddItem(new1);
+         //   TestModel.DeleteItem(typeof(NewTable), "1");
+        }
+    }
+}
---
+++ MysqlEntityFramework/MysqlEntityFramework/TestModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MysqlEntityFramework.test1;
+
+namespace MysqlEntityFramework
+{
+    public class TestModel
+    {
+
+        public static readonly test_1Context test_1Context = new test_1Context();
+
+        public static void AddItem(object item)
+        {
+            try
+            {
+                test_1Context.Add(item);
+                test_1Context.SaveChanges();
+                Console.WriteLine("添加成功！");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"操作失败！  {e.Message}");
+            }
+        }
+
+        public static void DeleteItem(Type type, string id)
+        {
+            try
+            {
+                var ans = test_1Context.Find(type, id);
+                test_1Context.Remove(ans);
+                test_1Context.SaveChanges();
+                Console.WriteLine("Operation Successed!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Operation failed! -------{e.Message}");
+            }
+        }
+            
+    }
+}
